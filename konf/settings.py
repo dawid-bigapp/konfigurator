72fcaa42318713f9f208be0b28bf298bdd802a60
Dawid Złotowski <dawid4727@gmail.com>
2023-05-09 12:50:41 +0200

    bledny host

--- konf/settings.py
+++ konf/settings.py
@@ -21,8 +21,8 @@ SECRET_KEY = "django-insecure-%%wg!)!1y*^6&=s=^ejy*70=8$2i3o7a*lg@p^i&-(7=44%@@j
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['*']
-# ALLOWED_HOSTS = ['konfigurator.onrender.com']
+# ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['konfigurator.onrender.com']
 
 
 # Application definition
